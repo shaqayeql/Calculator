c95442d9dd74f46ea2c6f4f37121a31f427e26a1
unknown <shaghayeghladani@yahoo.com>
2023-04-07 02:43:56 +0430

    add Action Listener

--- src/com/maktabkhoone/Calculator.java
+++ src/com/maktabkhoone/Calculator.java
@@ -2,8 +2,10 @@ package com.maktabkhoone;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Calculator {
+public class Calculator implements ActionListener {
 
     JFrame frame;
 
@@ -20,6 +22,8 @@ public class Calculator {
     public void createButton(String buttonName , int buttonX , int buttonY , int buttonWidth , int buttonHeight){
         JButton button = new JButton(buttonName);
         button.setBounds(buttonX, buttonY , buttonWidth , buttonHeight );
+        button.addActionListener(this);
+        button.setName(buttonName);
         frame.add(button);
     }
 
@@ -27,5 +31,65 @@ public class Calculator {
         JTextField text = new JTextField();
         text.setBounds(textX , textY , textWidth , textHeight);
         frame.add(text);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton button = (JButton) e.getSource();
+        if(button.getName() == "1"){
+            System.out.println("1");
+        }
+        if(button.getName() == "2"){
+            System.out.println("2");
+        }
+        if(button.getName() == "3"){
+            System.out.println("3");
+        }
+        if(button.getName() == "4"){
+            System.out.println("4");
+        }
+        if(button.getName() == "5"){
+            System.out.println("5");
+        }
+        if(button.getName() == "6"){
+            System.out.println("6");
+        }
+        if(button.getName() == "7"){
+            System.out.println("7");
+        }
+        if(button.getName() == "8"){
+            System.out.println("8");
+        }
+        if(button.getName() == "9"){
+            System.out.println("9");
+        }
+        if(button.getName() == "+"){
+            System.out.println("+");
+        }
+        if(button.getName() == "-"){
+            System.out.println("-");
+        }
+        if(button.getName() == "*"){
+            System.out.println("*");
+        }
+        if(button.getName() == "/"){
+            System.out.println("/");
+        }
+        if(button.getName() == "0"){
+            System.out.println("0");
+        }
+        if(button.getName() == "."){
+            System.out.println(".");
+        }
+        if(button.getName() == "="){
+            System.out.println("=");
+        }
+        if(button.getName() == "Delete"){
+            System.out.println("Delete");
+        }
+        if(button.getName() == "Clear"){
+            System.out.println("Clear");
+        }
+
     }
 }
