dbb890da3b6963dbd051d4a4eb03c92d22f0bdd4
unknown <shaghayeghladani@yahoo.com>
2023-04-13 23:09:54 +0430

    add functions

--- src/com/maktabkhoone/Calculator.java
+++ src/com/maktabkhoone/Calculator.java
@@ -8,6 +8,10 @@ import java.awt.event.ActionListener;
 public class Calculator implements ActionListener {
 
     JFrame frame;
+    JTextField text;
+    String operator = "";
+    double input1;
+    double input2;
 
     public void setupFrame(){
         frame = new JFrame("Calculator");
@@ -28,7 +32,7 @@ public class Calculator implements ActionListener {
     }
 
     public void createTextBox(int textX , int textY , int textWidth , int textHeight){
-        JTextField text = new JTextField();
+        text = new JTextField();
         text.setBounds(textX , textY , textWidth , textHeight);
         frame.add(text);
     }
@@ -37,58 +41,89 @@ public class Calculator implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         JButton button = (JButton) e.getSource();
         if(button.getName() == "1"){
-            System.out.println("1");
+            text.setText(text.getText().concat("1"));
         }
         if(button.getName() == "2"){
-            System.out.println("2");
+            text.setText(text.getText().concat("2"));
         }
         if(button.getName() == "3"){
-            System.out.println("3");
+            text.setText(text.getText().concat("3"));
         }
         if(button.getName() == "4"){
-            System.out.println("4");
+            text.setText(text.getText().concat("4"));
         }
         if(button.getName() == "5"){
-            System.out.println("5");
+            text.setText(text.getText().concat("5"));
         }
         if(button.getName() == "6"){
-            System.out.println("6");
+            text.setText(text.getText().concat("6"));
         }
         if(button.getName() == "7"){
-            System.out.println("7");
+            text.setText(text.getText().concat("7"));
         }
         if(button.getName() == "8"){
-            System.out.println("8");
+            text.setText(text.getText().concat("8"));
         }
         if(button.getName() == "9"){
-            System.out.println("9");
+            text.setText(text.getText().concat("9"));
         }
         if(button.getName() == "+"){
-            System.out.println("+");
+            input1 = Double.parseDouble(text.getText());
+            operator = "+";
+            text.setText("");
         }
         if(button.getName() == "-"){
-            System.out.println("-");
+            input1 = Double.parseDouble(text.getText());
+            operator = "-";
+            text.setText("");
         }
         if(button.getName() == "*"){
-            System.out.println("*");
+            input1 = Double.parseDouble(text.getText());
+            operator = "*";
+            text.setText("");
         }
         if(button.getName() == "/"){
-            System.out.println("/");
+            input1 = Double.parseDouble(text.getText());
+            operator = "/";
+            text.setText("");
         }
         if(button.getName() == "0"){
-            System.out.println("0");
+            text.setText(text.getText().concat("0"));
         }
         if(button.getName() == "."){
-            System.out.println(".");
+            text.setText(text.getText().concat("."));
         }
         if(button.getName() == "="){
-            System.out.println("=");
+            input2 = Double.parseDouble(text.getText());
+            double result = 0;
+            if (operator == "+"){
+                result = input1 + input2;
+                text.setText(result + "");
+            }
+            if (operator == "-"){
+                result = input1 - input2;
+                text.setText(result + "");
+            }
+            if (operator == "/"){
+                result = input1 / input2;
+                text.setText(result + "");
+            }
+            if (operator == "*"){
+                result = input1 * input2;
+                text.setText(result + "");
+            }
         }
         if(button.getName() == "Delete"){
-            System.out.println("Delete");
+            String content = text.getText();
+            text.setText("");
+            for (int i = 0; i < content.length() - 1; i++) {
+                text.setText(text.getText()+content.charAt(i));
+            }
+
         }
         if(button.getName() == "Clear"){
-            System.out.println("Clear");
+            text.setText("");
+            operator = "";
         }
 
     }
